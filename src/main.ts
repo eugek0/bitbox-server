b83791632a9bf709416eec19fa8ef52030d35c55
eugek0 <jmaster21042005@mail.ru>
2025-05-06 22:32:14 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({
     credentials: true,
-    origin,
+    origin: origin.split(","),
   });
 
   app.useGlobalPipes(new ValidationPipe());
